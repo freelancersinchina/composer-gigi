04f8117ceb240f7316822677e7ffda1907696348
freelancersinchina <openclazz@163.com>
2017-04-19 01:37:25 +0800

    fix bugs

--- src/GigiRandom.php
+++ src/GigiRandom.php
@@ -5,7 +5,7 @@ use \Metowolf\Meting;
 
 class GigiRandom extends MusicProvider{
 	public function getFirst($num){
-		$data = $this->api->search($this->singer);
+		$data = $this->api->search($this->singerName);
 
 		shuffle($data);
 		return array_slice($data,0,$num);
--- src/GigiSequence.php
+++ src/GigiSequence.php
@@ -4,7 +4,7 @@ namespace Gigi;
 
 class GigiSequence extends MusicProvider{
 	public function getFirst($num){
-		$data = $this->api->search($this->singer);
+		$data = $this->api->search($this->singerName);
 		return array_slice($data,0,$num);
 	}
 }
